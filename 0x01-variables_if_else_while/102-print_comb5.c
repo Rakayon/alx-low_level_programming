111cb74f6ff462229128166ae90793bf17bc8df3
Rakayon <aizeagbo@gmail.com>
2022-03-13 09:37:31 -0700

    my commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,20 +20,19 @@ int main(void)
 	{
 		b = a + 1;
 		while (b <= 99)
-		{	
-
+		{
 			putchar((a / 10) + n1);
 			putchar((a % 10) + n1);
 			putchar(32);
 			putchar((b / 10) + n1);
 			putchar((b % 10) + n1);
- 			if (a != 98 || b != 99)
+			if (a != 98 || b != 99)
 			{
 				putchar(com);
 				putchar(32);
-			}	
-			b += 1;
-		}	
+			}
+			b +=1;
+		}
 		a += 1;
 	}
 	putchar('\n');
