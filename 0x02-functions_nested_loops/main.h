7eaa058272223422ba4131689bdfa9fd4235ff57
Rakayon <aizeagbo@gmail.com>
2022-03-15 15:13:23 -0700

    my commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+@@ -1,5 +1,5 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #ifndef MAIN_H
@@ -5,9 +6,10 @@
 
 #include <stdio.h>
 
-@@ -8,4 +8,4 @@ int largest_number(int a, int b, int c);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 #endif /* HOLBERTON_H */
-#endif /* MAIN_H *
+#endif /* MAIN_H */
