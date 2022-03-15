9d5b4ad9e556e2bbb7377e011e5944e5d279a505
Rakayon <aizeagbo@gmail.com>
2022-03-15 16:45:41 -0700

    my commit

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,9 +13,11 @@ int main(void)
 
 	while (j < 4000000)
 	{
-	if (j % 2 == 0)
+		if (j % 2 == 0)
 		total += j;
-		k = j;										j += i;										i = k;
+		k = j;
+		j += i;
+		i = k;
 	}
 	printf("%d\n", total);
 	return (0);
